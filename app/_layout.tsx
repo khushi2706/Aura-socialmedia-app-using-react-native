4a95ed4812c63b1b940bbf5615aa2a5020a299f6
Khushi Patel <kdpatel@ibaset.com>
2024-10-22 13:47:16 +0530

    home page and create video added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -40,10 +40,10 @@ const RootLayout = () => {
   <>
    <GlobalProvider>
     <Stack>
-     {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} /> */}
+     <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
      <Stack.Screen name='(auth)' options={{ headerShown: false }} />
      <Stack.Screen name='index' options={{ headerShown: false }} />
-     {/* <Stack.Screen name="search/[query]" options={{ headerShown: false }} /> */}
+     <Stack.Screen name='search/[query]' options={{ headerShown: false }} />
     </Stack>
    </GlobalProvider>
   </>
